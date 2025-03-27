57d2d4575dcf04c3d6979fc123ce57bf6790fcde
Manuel Reinhardt <reinhardt@syslab.com>
2025-03-27 14:35:10 +0100

    Back to development: 1.0.0a6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="plonestatic.euphorie",
-    version="1.0.0a5",
+    version="1.0.0a6.dev0",
     description="Euphorie Risk Assessment tool static resources",
     long_description="\n".join(
         [
