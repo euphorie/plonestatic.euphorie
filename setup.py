1e21c11d4d05c32f89d40305f910dec99d3dadd5
ale-rt <alessandro.pisa@gmail.com>
2025-07-15 10:37:11 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="plonestatic.euphorie",
-    version="1.0.0",
+    version="1.0.1.dev0",
     description="Euphorie Risk Assessment tool static resources",
     long_description="\n".join(
         [
